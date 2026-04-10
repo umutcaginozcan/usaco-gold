29299beec723720a19ddd00b8201daa26edb23c2
Umut Çağın Özcan <umutcaginozcan@gmail.com>
2026-04-10 22:52:51 +0300

    graph-theory: LC787
    (dijkstra) test cases 59/59

--- graph-theory/LC787.cpp
+++ graph-theory/LC787.cpp
@@ -2,35 +2,46 @@
 LC787 Cheapest Flights Within K Stops
 */
 class Solution {
-#define INF 10000
 public:
   int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst,
                         int k) {
-    vector<int> dist(n, INF);
-    dist[src] = 0;
 
-    priority_queue<array<int, 2>, vector<array<int, 2>>, greater<array<int, 2>>>
+    vector<vector<array<int, 2>>> adjList(n);
+    for (auto e : flights) {
+      int u = e[0], v = e[1], w = e[2];
+      adjList[u].push_back({v, w});
+    }
+
+    vector<vector<int>> dist(n, vector<int>(k + 2, 1e6));
+    dist[src][0] = 0;
+
+    // dist, node id, k
+    priority_queue<array<int, 3>, vector<array<int, 3>>, greater<array<int, 3>>>
         pq;
-    pq.push({0, src});
-    while (k-- > -1 && !pq.empty()) {
-      int cur_dist = pq.top()[0];
-      int cur_node = pq.top()[1];
+    pq.push({0, src, 0});
+
+    while (!pq.empty()) {
+      int d = pq.top()[0];
+      int u = pq.top()[1];
+      int cnt = pq.top()[2];
       pq.pop();
 
-      for (auto flight : flights) {
-        if (flight[0] != cur_node)
-          continue;
-        int next_node = flight[1];
-        int cost = flight[2];
-        if (dist[next_node] > cost + cur_dist) {
-          dist[next_node] = cost + cur_dist;
-          pq.push({dist[next_node], next_node});
+      if (cnt == k + 1)
+        continue;
+      if (d > dist[u][cnt])
+        continue;
+
+      for (auto e : adjList[u]) {
+        int v = e[0], w = e[1];
+
+        if (dist[v][cnt + 1] > dist[u][cnt] + w) {
+          dist[v][cnt + 1] = dist[u][cnt] + w;
+          pq.push({dist[v][cnt + 1], v, cnt + 1});
         }
       }
     }
 
-    if (dist[dst] == INF)
-      return -1;
-    return dist[dst];
+    int ans = *min_element(dist[dst].begin(), dist[dst].end());
+    return ans == 1e6 ? -1 : ans;
   }
 };
